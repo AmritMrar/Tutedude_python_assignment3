38e681009d835b4cee8928a469d4a80f196ce980
AmritMrar <66363879+AmritMrar@users.noreply.github.com>
2025-08-27 21:09:46 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,11 @@
+import math
+number = float(input("Enter a number: "))
+
+square_root = math.sqrt(number)
+log = math.log(number)
+sine = math.sin(number)
+
+print(f"\n Results for the number {number}")
+print(f"Square root: {square_root}")
+print(f"logarithm: {log}")
+print(f"Sine: {sine}")
